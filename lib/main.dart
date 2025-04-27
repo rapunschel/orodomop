4f59381c578a3bef6bf60d5186978a6bd8b98c4b
rapunschel <rapunschel.gaming@gmail.com>
2025-04-27 23:23:42 +0200

    More spaghetti with buttons. Base functionality

--- lib/main.dart
+++ lib/main.dart
@@ -36,25 +36,97 @@ class TimerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            StopWatchWidget(),
-            SizedBox(height: 16),
-            Row(
+    return Consumer<TimerModel>(
+      builder: (context, model, child) {
+        return Scaffold(
+          body: Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                PauseOrResumeButton(),
-                SizedBox(width: 16),
-                ElevatedButton(onPressed: () {}, child: Text("Break")),
+                model.isOnBreak ? CountDownWatch() : CustomStopWatch(),
+                SizedBox(height: 16),
+                TimerControlRow(),
               ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class TimerControlRow extends StatelessWidget {
+  const TimerControlRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = context.read<TimerModel>();
+
+    if (model.focusTime == 0 && model.breakTimeRemaining == 0) {
+      return StartButton();
+    }
+
+    if (model.isOnBreak) {
+      return EndBreakButton();
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [PauseOrResumeButton(), SizedBox(width: 16), BreakButton()],
+    );
+  }
+}
+
+class BreakButton extends StatelessWidget {
+  const BreakButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        // Trigger the dialog to show up
+        _showBreakDialog(context);
+      },
+      child: Text("Break"),
+    );
+  }
+
+  // Function to show the dialog
+  void _showBreakDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return BreakDialog();
+      },
+    );
+  }
+}
+
+class EndBreakButton extends StatelessWidget {
+  const EndBreakButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        context.read<TimerModel>().resetTimer();
+      },
+      child: Text("Stop"),
+    );
+  }
+}
+
+class StartButton extends StatelessWidget {
+  const StartButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        context.read<TimerModel>().start();
+      },
+      child: Text("Start"),
     );
   }
 }
@@ -85,8 +157,40 @@ class PauseOrResumeButton extends StatelessWidget {
   }
 }
 
-class StopWatchWidget extends StatelessWidget {
-  const StopWatchWidget({super.key});
+class CountDownWatch extends StatelessWidget {
+  const CountDownWatch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<TimerModel, int>(
+      selector: (context, timerModel) => timerModel.breakTimeRemaining,
+      builder: (context, time, child) {
+        return Container(
+          width: 250,
+          height: 250,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(width: 3),
+          ),
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(32, 0, 32, 0),
+              child: AutoSizeText(
+                TimerModel.formatTime(time),
+                maxLines: 1,
+                maxFontSize: 68,
+                style: TextStyle(fontSize: 68),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class CustomStopWatch extends StatelessWidget {
+  const CustomStopWatch({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -113,6 +217,48 @@ class StopWatchWidget extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class BreakDialog extends StatelessWidget {
+  BreakDialog();
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+
+    return AlertDialog(
+      title: Text("Set Break Duration (1/X)"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: controller,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(hintText: 'Enter X'),
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            final value = controller.text;
+            if (value.isNotEmpty) {
+              int x = int.tryParse(value) ?? 1; // Default to 1 if invalid
+              context.read<TimerModel>().relax(x);
+            }
+            Navigator.of(context).pop();
+          },
+          child: Text("OK"),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(); // Just close the dialog
+          },
+          child: Text("Cancel"),
+        ),
+      ],
     );
   }
 }
