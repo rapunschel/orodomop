bc312f9364ef515843c9abc230774d9c9370edd1
rapunschel <rapunschel.gaming@gmail.com>
2025-04-27 16:37:36 +0200

    reinit

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 244, 106, 8),
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
