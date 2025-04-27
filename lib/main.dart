c92c5a34e9a7af43e531146aeeaae1223c4e80f4
rapunschel <rapunschel.gaming@gmail.com>
2025-04-27 20:52:59 +0200

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:provider/provider.dart';
+import 'package:orodomop/timer_model.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final timerModel = await TimerModel.create();
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => timerModel)],
+      child: OrodomopApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class OrodomopApp extends StatelessWidget {
+  const OrodomopApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    debugPrint("Restart");
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Orodomop',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
@@ -32,25 +42,7 @@ class TimerScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: 250,
-              height: 250,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(width: 3),
-              ),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(32, 0, 32, 0),
-                  child: AutoSizeText(
-                    "99:33:57",
-                    maxLines: 1,
-                    maxFontSize: 68,
-                    style: TextStyle(fontSize: 68),
-                  ),
-                ),
-              ),
-            ),
+            StopWatchWidget(),
             SizedBox(height: 16),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -63,6 +55,37 @@ class TimerScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class StopWatchWidget extends StatelessWidget {
+  const StopWatchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TimerModel>(
+      builder: (context, timerModel, child) {
+        return Container(
+          width: 250,
+          height: 250,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(width: 3),
+          ),
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(32, 0, 32, 0),
+              child: AutoSizeText(
+                timerModel.focusTime,
+                maxLines: 1,
+                maxFontSize: 68,
+                style: TextStyle(fontSize: 68),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
