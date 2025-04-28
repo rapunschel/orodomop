86d97ab85142dd11de79fc0a6da219440a45485b
rapunschel <rapunschel.gaming@gmail.com>
2025-04-28 15:27:58 +0200

    Changed to only listen to breaktime and not entire model

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -10,17 +10,16 @@ class TimerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<TimerModel>(
-      builder: (context, model, child) {
+    return Selector<TimerModel, int>(
+      selector: (context, model) => model.breakTimeRemaining,
+      builder: (context, breakTime, child) {
         return Scaffold(
           body: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                model.breakTimeRemaining > 0
-                    ? CountDownWatch()
-                    : CustomStopWatch(),
+                breakTime > 0 ? CountDownWatch() : CustomStopWatch(),
                 SizedBox(height: 16),
                 TimerControlRow(),
               ],
