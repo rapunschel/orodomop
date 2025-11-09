b41c42b2e136fd2657e280e4cb4d3e6f68f61030
rapunschel <93683352+rapunschel@users.noreply.github.com>
2025-11-09 16:41:56 +0100

    Replace isCounting bool with TimerState enum

--- lib/models/timer_model.dart
+++ lib/models/timer_model.dart
@@ -4,33 +4,54 @@ import 'dart:async';
 import 'package:orodomop/services/service_manager.dart';
 import 'package:orodomop/services/notification_service.dart';
 
+enum TimerState {
+  idle,
+  onFocus,
+  onBreak,
+  paused;
+
+  get isOnFocus => this == onFocus;
+  get isIdle => this == idle;
+  get isOnBreak => this == onBreak;
+  get isPaused => this == paused;
+
+  static TimerState fromString(String? timerState) {
+    return TimerState.values.firstWhere(
+      (state) => state.name == timerState,
+      orElse: () => TimerState.idle,
+    );
+  }
+}
+
 class TimerModel with ChangeNotifier {
+  final SharedPreferences _prefs;
   int _focusTime;
   int _breakTimeRemaining;
-  bool _isCounting;
-  final SharedPreferences _prefs;
   Timer? _timer;
+  TimerState _timerState;
+
   TimerModel._(
     this._focusTime,
     this._breakTimeRemaining,
-    this._isCounting,
+    this._timerState,
     this._prefs,
   );
 
-  // factory constructor to load data.
   static Future<TimerModel> create() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     int breakTimeRemaining = prefs.getInt("breakTimeRemaining") ?? 0;
     int focusTime = prefs.getInt("focusTime") ?? 0;
-    bool isCounting = prefs.getBool("isCounting") ?? false;
     String timestamp = prefs.getString("timestamp") ?? "";
+    TimerState timerState = TimerState.fromString(
+      prefs.getString("timerState"),
+    );
 
     if (timestamp.isEmpty) {
       // empty, avoid formatexception when calling DateTime.parse
-    } else if (isCounting) {
+    } else if (timerState.isOnFocus) {
       focusTime +=
           DateTime.now().difference(DateTime.parse(timestamp)).inSeconds;
-    } else if (breakTimeRemaining > 0) {
+    } else if (timerState.isOnBreak) {
       breakTimeRemaining -=
           DateTime.now().difference(DateTime.parse(timestamp)).inSeconds;
 
@@ -44,7 +65,7 @@ class TimerModel with ChangeNotifier {
     TimerModel model = TimerModel._(
       focusTime,
       breakTimeRemaining,
-      isCounting,
+      timerState,
       prefs,
     );
 
@@ -54,28 +75,28 @@ class TimerModel with ChangeNotifier {
   Future<void> saveState() async {
     await _prefs.setInt("breakTimeRemaining", _breakTimeRemaining);
     await _prefs.setInt("focusTime", _focusTime);
-    await _prefs.setBool("isCounting", _isCounting);
     await _prefs.setString("timestamp", DateTime.now().toString());
+    await _prefs.setString("timerState", _timerState.name);
   }
 
   Future<void> clearPrefs() async {
     await _prefs.remove("breakTimeRemaining");
     await _prefs.remove("focusTime");
-    await _prefs.remove("isCounting");
     await _prefs.remove("timestamp");
+    await _prefs.remove("timerState");
   }
 
   void start() {
     try {
       // Avoid starting new timers, unless neccessary
       // example: app restart, this ensures timer is started when calling start
-      if (_isCounting && _timer != null) {
+      if (_timerState.isOnFocus && _timer != null) {
         return;
       }
+      _timerState = TimerState.onFocus;
 
       // Cancel in case notif was shown. No longer needed
       NotificationService().cancelNotification(NotificationId.scheduledNotif);
-      _isCounting = true;
       ServiceManager.startService(); // Start the foreground service
 
       _timer = Timer.periodic(Duration(seconds: 1), (timer) {
@@ -90,7 +111,7 @@ class TimerModel with ChangeNotifier {
   }
 
   void onAppRestart() {
-    if (isCounting) {
+    if (_timerState.isOnFocus) {
       start();
     } else if (breakTimeRemaining > 0) {
       _countDownTimer();
@@ -110,33 +131,30 @@ class TimerModel with ChangeNotifier {
     });
   }
 
-  void _stopTimer() {
-    _isCounting = false;
-    _timer?.cancel();
-  }
-
   void resetTimer() async {
-    await clearPrefs(); // Reset sharedpreferences
-    _stopTimer();
+    await clearPrefs();
+    _timer?.cancel();
     _focusTime = 0;
     _breakTimeRemaining = 0;
+    _timerState = TimerState.idle;
     ServiceManager.stopService();
     notifyListeners();
   }
 
   void resume() {
+    // TODO Implement logic to check which timer to resume
     start();
-    notifyListeners();
   }
 
   void pause() {
-    _stopTimer();
+    _timer?.cancel();
+    _timerState = TimerState.paused;
     ServiceManager.stopService();
     notifyListeners();
   }
 
   void relax(int x) {
-    _stopTimer(); // Stop the timer in case it's running.
+    _timer?.cancel(); // Stop the timer in case it's running.
     _breakTimeRemaining = (_focusTime / x).round();
 
     NotificationService().scheduleBreakNotification(
@@ -145,6 +163,7 @@ class TimerModel with ChangeNotifier {
     );
 
     _focusTime = 0;
+    _timerState = TimerState.onBreak;
     _countDownTimer();
     notifyListeners();
   }
@@ -154,8 +173,8 @@ class TimerModel with ChangeNotifier {
     NotificationService().cancelNotification(NotificationId.scheduledNotif);
   }
 
-  get isActive => _isCounting || _focusTime > 0;
   get focusTime => _focusTime;
-  get isCounting => _isCounting;
+  get isCounting => _timerState.isOnFocus; // TODO update
   get breakTimeRemaining => _breakTimeRemaining;
+  get timerState => _timerState;
 }
