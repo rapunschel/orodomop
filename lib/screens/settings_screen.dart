7a91df901bb136b3a5bef7bc1639635f63337327
rapunschel <93683352+rapunschel@users.noreply.github.com>
2025-11-18 23:52:14 +0100

    refactor use settings widgets

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:orodomop/widgets/buttons/toggle_theme_button.dart';
+import 'package:orodomop/widgets/settings/settings_item.dart';
+import 'package:orodomop/widgets/settings/settings_section_title.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
@@ -7,14 +9,24 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Settings")),
+      appBar: AppBar(
+        title: Text(
+          "Settings",
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+            fontSize: 36,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 36),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text("Dark mode "), ToggleThemeButton()],
+            // User interface settings
+            Column(
+              children: [
+                SettingsSectionTitle(title: "UI Settings"),
+                SettingsItem(text: "Dark mode", widget: ToggleThemeButton()),
+              ],
             ),
           ],
         ),
