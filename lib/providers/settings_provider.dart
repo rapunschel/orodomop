73f7c26109de47c5bc0c83275fb699b98554fcae
rapunschel <93683352+rapunschel@users.noreply.github.com>
2025-11-22 01:21:41 +0100

    added callback to notify TimerProvider. Variables for focus/break duration

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -3,15 +3,32 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider with ChangeNotifier {
   final SharedPreferences _prefs;
+  Function? onUsePomodoroCallback;
   static SettingsProvider? _instance;
   bool _usePomodoro = false;
-  SettingsProvider._(this._usePomodoro, this._prefs);
+  int _focusDuration;
+  int _breakDuration;
+
+  SettingsProvider._(
+    this._usePomodoro,
+    this._prefs,
+    this._focusDuration,
+    this._breakDuration,
+  );
 
   static SettingsProvider getInstance(SharedPreferences prefs) {
     if (_instance != null) return _instance!;
     bool usePomodoro = prefs.getBool("usePomodoro") ?? false;
+    // TODO update with proper values after testing
+    int focusDuration = prefs.getInt("focusDuration") ?? 6;
+    int breakDuration = prefs.getInt("breakDuration") ?? 3;
 
-    _instance = SettingsProvider._(usePomodoro, prefs);
+    _instance = SettingsProvider._(
+      usePomodoro,
+      prefs,
+      focusDuration,
+      breakDuration,
+    );
     return _instance!;
   }
 
@@ -19,9 +36,24 @@ class SettingsProvider with ChangeNotifier {
     if (_usePomodoro == value) return;
 
     _usePomodoro = value;
+    onUsePomodoroCallback?.call();
     _prefs.setBool("usePomodoro", _usePomodoro);
     notifyListeners();
   }
 
+  set focusDuration(int value) {
+    _focusDuration = value;
+    _prefs.setInt("focusDuration", _focusDuration);
+    notifyListeners();
+  }
+
+  set breakDuration(int value) {
+    _breakDuration = value;
+    _prefs.setInt("breakDuration", _breakDuration);
+    notifyListeners();
+  }
+
   bool get usePomodoro => _usePomodoro;
+  int get focusDuration => _focusDuration;
+  int get breakDuration => _breakDuration;
 }
