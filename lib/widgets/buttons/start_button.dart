6fd8617fac79cb54553a02c8a2db098cde5018bd
rapunschel <93683352+rapunschel@users.noreply.github.com>
2025-11-22 15:28:21 +0100

    rename: button text from start -> focus

--- lib/widgets/buttons/start_button.dart
+++ lib/widgets/buttons/start_button.dart
@@ -11,7 +11,7 @@ class StartButton extends StatelessWidget {
       onPressed: () {
         context.read<TimerProvider>().startFocusTimer();
       },
-      child: Text("Start"),
+      child: Text("Focus"),
     );
   }
 }
