6269b3095d8caf110df3c9dd4e8d4c48a47fd51b
rapunschel <93683352+rapunschel@users.noreply.github.com>
2025-11-23 01:47:31 +0100

    feat: settings for break reminders for orodomop

--- lib/screens/settings_screen/sections/orodomop_settings_section.dart
+++ lib/screens/settings_screen/sections/orodomop_settings_section.dart
@@ -3,6 +3,7 @@ import 'package:orodomop/providers/settings_provider.dart';
 import 'package:orodomop/screens/settings_screen/widgets/settings_item.dart';
 import 'package:orodomop/screens/settings_screen/widgets/settings_section_title.dart';
 import 'package:provider/provider.dart';
+import 'package:orodomop/screens/settings_screen/widgets/settings_duration_text_form.dart';
 
 class OrodomopSettingsSection extends StatelessWidget {
   const OrodomopSettingsSection({super.key, required this.model});
@@ -10,18 +11,44 @@ class OrodomopSettingsSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Selector<SettingsProvider, bool>(
-      selector: (context, model) => model.rememberX,
-      builder: (context, rememberX, child) {
+    return Selector<
+      SettingsProvider,
+      ({bool rememberX, bool reminderEnabled, int breakReminderSeconds})
+    >(
+      selector:
+          (context, model) => (
+            rememberX: model.rememberX,
+            reminderEnabled: model.breakReminderEnabled,
+            breakReminderSeconds: model.breakReminderSeconds,
+          ),
+      builder: (context, values, child) {
         return Column(
           children: [
             SettingsSectionTitle(title: "Orodomop"),
             SettingsItem(
               text: "Remember X",
               widget: Switch(
-                value: rememberX,
+                value: values.rememberX,
                 onChanged: (value) {
                   model.rememberX = value;
+                },
+              ),
+            ),
+            SettingsItem(
+              text: "Enable break reminder",
+              widget: Switch(
+                value: values.reminderEnabled,
+                onChanged: (value) {
+                  model.breakReminderEnabled = value;
+                },
+              ),
+            ),
+            SettingsItem(
+              text: "Break reminder (m)",
+              widget: SettingsDurationTextForm(
+                seconds: values.breakReminderSeconds,
+                onSubmit: ({required seconds}) {
+                  model.breakReminderSeconds = seconds;
                 },
               ),
             ),
